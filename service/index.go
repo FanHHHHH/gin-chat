9defff0af712c712dbde63e2aba673bbfcdac241
leofanhe <leofanhe@tencent.com>
2023-03-11 18:37:44 +0800

    feat: swagger

--- service/index.go
+++ service/index.go
@@ -6,9 +6,13 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// GetIndex
+// @Tags 首页
+// @Success 200 {string} json "{"code":200,"data":"welcome"}"
+// @Router /index [get]
 func GetIndex(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
-		"message": "welcome",
+		"data": "welcome",
 	})
 }
 
--- service/user_service.go
+++ service/user_service.go
@@ -7,10 +7,14 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// GetUserList
+// @Tags User
+// @Success 200 {string} json{"code","data"}
+// @Router /user/getUserList [get]
 func GetUserList(c *gin.Context) {
 	users := make([]*models.UserBasics, 10)
 	users = models.GetUserList()
 	c.JSON(http.StatusOK, gin.H{
-		"message": users,
+		"data": users,
 	})
 }
